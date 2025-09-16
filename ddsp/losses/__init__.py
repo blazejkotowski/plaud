4f7e563812a11329f1e1fa39616b883a4a326763
Błażej Kotowski <blazej.kotowski@upf.edu>
2025-09-16 17:13:11 +0200

    do not include some exotic losses

--- ddsp/losses/__init__.py
+++ ddsp/losses/__init__.py
@@ -1,5 +1,5 @@
-from .m2l_loss import M2LLoss
-from .clap_loss import CLAPLoss
+# from .m2l_loss import M2LLoss
+# from .clap_loss import CLAPLoss
 from .attribute_regularization_loss import AttributeRegularizationLoss
 from .sliced_wasserstein_loss import SlicedWassersteinLoss
 from .multi_scale_sliced_wasserstein_loss import MultiScaleSlicedWassersteinLoss
